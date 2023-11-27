3dbcb67ab5f6d40689b6bc63dd2bf8f0e9fad2e5
thecoder-co <thecoder.co@gmail.com>
2023-11-27 14:25:56 +0100

    new data

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_restful import *
 import requests
 
 
-Location='London,uk'
+locations_list=['London,uk', "Lagos,ng"]
 
 APP_ID='9921065396d054a0f07bcfeb4bc50cd1'
 
@@ -12,26 +12,36 @@ api = Api(app)
 
 #CORS(app, resources={r"*": {"origins": "*"}})
 
-url='http://api.openweathermap.org/data/2.5/weather?q='+Location+'&APPID='+APP_ID
+url='http://api.openweathermap.org/data/2.5/weather?q='
 
+def get_res(location):
+    return requests.get(url + location+'&APPID='+APP_ID)
 
 class GetWeather(Resource):
     #@cross_origin()
     def get(self):
-        response= requests.get(url)
-        response.raise_for_status()
-        # get the identity from the jwt
-        weatherData = json.loads(response.text)
-        w=weatherData['weather']
-        print('Current weather in %s:' % (weatherData["name"]))
-        print(w[0]['description'])
-        print('The maximum temperature in Kelvin is %s:' % (weatherData["main"]["temp"]))
+        locations_data = []
+        for location in locations_list:
+            response = get_res(location)
+            response.raise_for_status()
+            locations_data.append(response.json())
 
-        return {'status_code': 200, 'body': weatherData, 
-        'current_weather': f'Current weather in {weatherData["name"]}: is {w[0]["description"]}',
-        'max_temp': 'The maximum temperature in Kelvin is %s:' % (weatherData["main"]["temp"]),
+        weatherData = json.loads(json.dumps(locations_data))
+        # w=weatherData['weather']
+        # print('Current weather in %s:' % (weatherData["name"]))
+        # print(w[0]['description'])
+        # print('The maximum temperature in Kelvin is %s:' % (weatherData["main"]["temp"]))
+        data = {'status_code': 200, 'body': weatherData,
+        }
+        for i in locations_data:
+            w=i['weather']
+            name = i['name']
+            description = w[0]['description']
+            temp = i['main']['temp']
+            data[f'current_weather_{name}'] = f'Current weather in {name}: is {description}'
+            data[f'max_temp_{name}'] = f'The maximum temperature in Kelvin is {temp}'
 
-        }, 200
+        return data, 200
 
 
 
